bb9151691878c9b2d9a1fa998190d8dbc3e39473
Jayanta66 <jayhalder18@gmail.com>
2025-06-29 10:12:59 +0530

    add

--- src/components/checkout/PaymentForm.jsx
+++ src/components/checkout/PaymentForm.jsx
@@ -20,8 +20,8 @@ const PaymentForm = ({ clientSecret, totalPrice }) => {
             elements,
             clientSecret,
             confirmParams: {
-                return_url: `https://luminous-gelato-af6d4d.netlify.app/order-confirm`,
-          //      return_url: `${import.meta.env.VITE_FRONTEND_URL}/order-confirm`,
+          //      return_url: `https://luminous-gelato-af6d4d.netlify.app/order-confirm`,
+                return_url: `${import.meta.env.VITE_FRONTEND_URL}/order-confirm`,
 
             },
         });
